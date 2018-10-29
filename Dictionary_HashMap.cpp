a91a5e7c522798ba9fcd20045859ee12f00e3d95
Diane deGraft-Johnson <dddegraft@gmail.com>
2018-10-29 09:46:35 -0700

    updated range check for number queries

--- Dictionary_HashMap.cpp
+++ Dictionary_HashMap.cpp
@@ -10,6 +10,7 @@ using namespace std;
 int main() {
   int noQueries;
   long phoneNum;
+  long noQueries = 1;
   
   string name, query;
   map <string, long>::iterator itDictionary;
@@ -19,7 +20,7 @@ int main() {
   
   map <string, long> phoneBook;
   
-  if ((noEntries >= 1) && (noEntries <= 10000)) {
+  if ((noEntries >= 1) && (noEntries <= 100000)) {
         for (int i = 1; i <= noEntries; i++) {
             cin >> name;
             cin >> phoneNum;
@@ -28,14 +29,18 @@ int main() {
         } // capture input until exceeds noEntries; first input integer
     
         for (auto &it : phoneBook) {
-          cin >> query;     
-          itDictionary = phoneBook.find(query); // if found, returns an iterator to the element
+          cin >> query;   
+          
+          if ((!query.empty()) && ((noQueries >= 1) && (noQueries <= 100000)) ) {
+                noQueries++;
+                itDictionary = phoneBook.find(query); // if found, returns an iterator to the element
                 
-          if (itDictionary != phoneBook.end()) { // if not found, returns map::end()
-              cout << itDictionary->first << "=" << itDictionary->second << endl;
-          } else {
-              cout << "Not found" << endl;
-          } 
+                if (itDictionary != phoneBook.end()) { // if not found, returns map::end()
+                    cout << itDictionary->first << "=" << itDictionary->second << endl;
+                } else {
+                    cout << "Not found" << endl;
+                } 
+          } // check ok range for number of queries
         } // iterating through the map data structure phoneBook
         
     } // end of if (...) checking successful range of noEntries
